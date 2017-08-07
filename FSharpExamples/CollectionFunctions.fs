fe6eaa245369a6995760a041f5fc5f3d7d2a68b0
Ryan Jackson <ryanjackson0y-@live.com>
2017-08-07 13:55:00 -0500

    Added 'CollectionFunctions' while going through chapter 01.

---
+++ FSharpExamples/CollectionFunctions.fs
@@ -0,0 +1,9 @@
+﻿namespace CollectionFunctions
+
+    module Sequence =
+
+        let lengthTest col =
+            col |> Seq.length
+
+        let sortTest col = 
+            col |> Seq.sort
--- FSharpExamples/Program.fs
+++ FSharpExamples/Program.fs
@@ -8,7 +8,9 @@ module Program =
     let main argv = 
         //printfn "%A" argv //accept program input arguments
 
-        printfn "sum value after compilation: %i" Chapter1.sum
+        printfn "Chapter 1: sum value after compilation: %i" Chapter1.sum
+
+        printfn "CollectionFunctions.lengthTest [ 1..10 ]: %i" (CollectionFunctions.Sequence.lengthTest [1..10])
 
         Console.ReadLine() |> ignore
         0 // return an integer exit code
