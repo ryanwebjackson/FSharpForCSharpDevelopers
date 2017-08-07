9de95c21678775d335bfd96f49b16f8f65ac864c
Jackson <ryjackson@affiniongroup.com>
2017-08-07 10:48:47 -0500

    Initial commit

---
+++ FSharpExamples/Chapter1.fs
@@ -0,0 +1,6 @@
+﻿module Chapter1
+
+let mutable sum = 0
+for i = 0 to 100 do
+    if i%2 <> 0 then sum <- sum + i
+printfn "the sum of odd numbers from 0 to 100 is %A" sum
---
+++ FSharpExamples/Program.fs
@@ -0,0 +1,7 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    Console.ReadLine() |> ignore
+    0 // return an integer exit code
