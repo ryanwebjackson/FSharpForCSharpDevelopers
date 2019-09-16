d2ef2abf5cc246249215233c0225b6d2e21b267a
Ryan Jackson <ryanjackson0y-@live.com>
2019-09-15 19:09:38 -0500

    Add Chapter 07, Listing 7-31.

---
+++ FSharpExamples/Chapter07.fs
@@ -0,0 +1,30 @@
+﻿// Listing 7-31: Finding the LCA (Lowest Common Ancestor) in a BST (Binary Search Tree)
+type NodeType = int
+
+type BinaryTree =
+    | Nil
+    | Node of NodeType * BinaryTree * BinaryTree
+
+let createBinaryTree() =
+    Node(20, 
+        Node(8, 
+            Node(5, Nil, Nil),
+            Node(12, Node(11, Nil, Nil), Node(15, Nil, Nil))
+            ),
+        Node(30, Node(21, Nil, Nil), Node(32, Nil, Nil))
+    )
+
+let tree = createBinaryTree()
+
+let rec findLCAinBST (n0, n1) root = 
+    match root with
+    | Nil -> None
+    | Node(v, left, right) ->
+        if v >= n0 && v <= n1 then
+            Some v
+        elif v > n0 && v > n1 then
+            findLCAinBST (n0, n1) left
+        else
+            findLCAinBST (n0, n1) right
+
+tree |> findLCAinBST (3, 15)
