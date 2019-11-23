c9f55310dac5dfd516aab4dc269eee548c3d94f0
Ryan Jackson <ryanjackson0y-@live.com>
2019-11-23 11:14:53 -0600

    Create Chapter08_Listing8-21.fs

---
+++ FSharpExamples/Chapter08_Listing8-21.fs
@@ -0,0 +1,8 @@
+/// Evolutionary computation module:
+/// this module is for Genetic Algorithm (GA) only
+module EvolutionaryComputation
+  
+  /// random number generator (RNG)
+  let random = new System.Random((int)System.DateTime.UtcNow.Ticks)
+  
+  
